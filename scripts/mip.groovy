9a8a849f5760db6b4fca6023ab555eaa91a75eae
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-02-18 14:54:32 +0100

    Split list choices by '#' (multiple possible)

--- scripts/mip.groovy
+++ scripts/mip.groovy
@@ -12,7 +12,7 @@ mip.addInput("6", "NoInput", new NoInputType())
 if (UIFactory.showDialog(mip)) {
     println "CheckBox checked: " + mip.getInput("1")
     println "ComboBoxChoice: " + mip.getInput("2")
-    println "ListChoice: " + mip.getInput("3")
+    println "ListChoice: " + mip.getInput("3").split("#")
     println "TextBoxChoice: " + mip.getInput("4")
     println "PasswordChoice: " + mip.getInput("5")
     println "NoInputChoice: " + mip.getInput("6")
