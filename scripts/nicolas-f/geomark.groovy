04501881bd4c996301d5ae21c960d5e85a6ed8a9
nicolas-f <github@nettrader.fr>
2014-02-19 11:34:09 +0100

    Geomark first commit

---
+++ scripts/nicolas-f/geomark.groovy
@@ -0,0 +1,92 @@
+import org.orbisgis.core.Services
+import org.orbisgis.viewapi.docking.*
+import javax.swing.*
+import org.orbisgis.viewapi.edition.*
+import groovy.swing.SwingBuilder
+import org.orbisgis.view.icons.OrbisGISIcon
+import org.orbisgis.mapeditorapi.MapElement;
+import org.orbisgis.core.layerModel.MapContext;
+import javax.swing.JList
+import org.apache.log4j.Logger;
+import com.vividsolutions.jts.geom.Envelope;
+
+/**
+ * A panel that manages map positionning
+ */
+class MyPanel implements EditorDockable {
+    Logger LOGGER = Logger.getLogger(MyPanel.class);
+    DockingPanelParameters dockingParameters = new DockingPanelParameters()
+    JPanel panel
+    SwingBuilder swing = new SwingBuilder()
+    MapContext mapContext
+    JList geoMarkList    
+    List listData = []
+     
+    MyPanel() {
+    	   EditorManager editorManager = Services.getService(EditorManager.class)
+    	   mapContext = MapElement.fetchFirstMapElement(editorManager).getMapContext();
+        dockingParameters.setName("geomark")
+        dockingParameters.setTitle("Geomark")
+        dockingParameters.setDockActions(
+                [swing.action(closure: { remove() }, smallIcon: OrbisGISIcon.getIcon("remove"), name: "Remove panel"),
+                 swing.action(closure: { addGeoMark() }, smallIcon: OrbisGISIcon.getIcon("add"), name: "Add GeoMark")
+                ])
+        panel = swing.panel() {
+            borderLayout()
+            scrollPane( constraints:CENTER ) {
+                geoMarkList = list(id:'listId')
+            }
+        }
+    }
+    
+    void addGeoMark() {
+    	 try {
+    	 	Envelope env = mapContext.getBoundingBox()
+    	 	if(env != null) {
+		 	listData.add(env.toString())
+     	 	geoMarkList.listData = listData
+    	 	}
+    	 } catch(Exception ex) {
+    	 	LOGGER.error(ex.getLocalizedMessage(), ex)
+    	 }
+    }
+    
+    void remove() {
+        EditorManager editorManager = Services.getService(EditorManager.class)
+        editorManager.removeEditor(this)
+    }
+
+    DockingPanelParameters getDockingParameters() {
+        return dockingParameters
+    }
+
+    JComponent getComponent() {
+        return panel
+    }
+
+    boolean match(org.orbisgis.viewapi.edition.EditableElement editableElement) {
+        return editableElement instanceof MapElement
+    }
+
+    EditableElement getEditableElement() {
+        return null
+    }
+
+    void setEditableElement(EditableElement editableElement) {
+		if(editableElement instanceof MapElement) {
+			mapContext = editableElement.getMapContext()
+		}
+    }
+}
+
+EditorManager editorManager = Services.getService(EditorManager.class)
+EditorDockable panel = new MyPanel()
+editorManager.addEditor(panel)
+
+
+
+
+
+
+
+
